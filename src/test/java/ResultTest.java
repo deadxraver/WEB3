c6a8aa32fddcd16724eb1bdd86ec43c3a14a79f7
deadxraver <409853@edu.itmo.ru>
2025-05-12 16:38:19 +0300

    all tests done so as the target test

---
+++ src/test/java/ResultTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ResultTest {
+  @Test
+  public void testResult() {
+    double x = 1, y = 0, r = 2;
+    Result result = new Result(x, y, r);
+    assertTrue(String.format("result for {x=%.1f,y=%.1f,r=%.1f} should be true!", x, y, r), result.resultAll());
+  }
+}
--- src/test/java/ValidatorTest.java
+++ src/test/java/ValidatorTest.java
@@ -6,5 +6,7 @@ public class ValidatorTest {
   public void testValidator() {
     double x = 0, y = 0, r = 2;
     assertTrue("{x=" + x + ",y=" + y + ",r=" + r + "} should be valid!!", Validator.validateAll(x, y, r));
+    r = 20;
+    assertFalse("{x=" + x + ",y=" + y + ",r=" + r + "} should not be valid!!", Validator.validateAll(x, y, r));
   }
 }

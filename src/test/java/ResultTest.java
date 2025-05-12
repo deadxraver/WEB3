1a02fd26449390e8c08a3a05ee6657b67c298547
deadxraver <409853@edu.itmo.ru>
2025-05-13 01:33:57 +0300

    Successful test

--- src/test/java/ResultTest.java
+++ src/test/java/ResultTest.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ResultTest {
-  @Test
+  @Test // Let's run some tests
   public void testResult() {
     double x = 1, y = 0, r = 2;
     Result result = new Result(x, y, r);
